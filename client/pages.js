4e4681a51d2cdec11e24bbf603ac1806daad8605
David Ascher <david.ascher@gmail.com>
2013-02-27 09:05:32 -0800

    / ‘client/pages.js’

--- client/pages.js
+++ client/pages.js
@@ -16,6 +16,7 @@ Template.newpara.events({
       'page': pageName,
       'content': ["Click on this paragraph to make it yours."]
     })
+    // XXX would be good to make that paragraph be selected right away.
   }
 })
 
