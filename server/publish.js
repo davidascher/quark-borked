63681b951ca80b9fffd55facce580284514dc481
David Ascher <david.ascher@gmail.com>
2013-02-27 18:07:32 -0800

    / ‘server/publish.js’

--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,7 @@
 // Pages -- {name: String}
 Pages = new Meteor.Collection("pages");
 Paras = new Meteor.Collection("paras");
-Redirects = new Meteor.Collection("paras");
+Redirects = new Meteor.Collection("redirects");
 
 // // Publish complete set of lists to all clients.
 // Meteor.publish('pages', function () {
