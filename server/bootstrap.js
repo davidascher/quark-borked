6e5880b9ebb58601b2c0d529f5f09ec4fbaba478
David Ascher <david.ascher@gmail.com>
2013-02-28 08:43:34 -0800

    / ‘server/bootstrap.js’

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,7 +1,6 @@
 var require = __meteor_bootstrap__.require;
 var fs = require('fs');
 var path = require('path');
-var stats = require('stats');
 // if the database is empty on server start, create some sample data.
 var bootstrap_data_path = './recipes';
 
