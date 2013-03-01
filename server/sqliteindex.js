1ad8586f42ed63d4ced474cfa5a65e99394c5653
David Ascher <david.ascher@gmail.com>
2013-03-01 10:42:26 -0800

    / ‘server/sqliteindex.js’

--- server/sqliteindex.js
+++ server/sqliteindex.js
@@ -4,7 +4,6 @@ var allparas = Paras.find();
 
 var onParaChange = {
 	added: function(id, fields) {
-		// console.log("added", id, fields);
 		Meteor._debug("added", id, fields);
 	},
 	changed: function(id, fields) {
