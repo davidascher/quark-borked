b86fd0a00431eeb14e05b967868e29bdfdc95ccb
David Ascher <david.ascher@gmail.com>
2013-03-01 13:42:18 -0800

    / ‘paragraphs.sqlite3’ / ‘server/sqliteindex.js’

--- server/sqliteindex.js
+++ server/sqliteindex.js
@@ -4,6 +4,32 @@ var sqlite3 = require('sqlite3');
 var allparas = Paras.find();
 var db;
 
+function searchForATerm() {
+	var fiber = Fiber(function() {
+	  	// SQL injection prevention code goes here. =()
+		db.run("SELECT key FROM paragraphs WHERE data MATCH '" + term + "'", function(err, rows) {
+	    	Meteor._debug(rows);
+			Fiber.yield(rows);
+		});
+	});
+	return fiber.run.bind(fiber);
+}
+
+Meteor.methods({
+  search: function (term) {
+  	return searchForATerm(term);
+    // 	Fiber.yield(rows);
+    // });
+    // if (you want to throw an error)
+    //   throw new Meteor.Error(404, "Can't find my pants");
+    // return "some return value";
+  },
+
+  bar: function () {
+    // .. do other stuff ..
+    return "baz";
+  }
+});
 
 var onParaChange = {
 	added: function(id, fields) {
