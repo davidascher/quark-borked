5a8ea9501941fd67a9058dd0769b9d36dc05e3f6
David Ascher <david.ascher@gmail.com>
2013-03-01 13:25:02 -0800

    / ‘server/sqliteindex.js’

--- server/sqliteindex.js
+++ server/sqliteindex.js
@@ -26,14 +26,13 @@ function setupSqlite() {
   	db = new sqlite3.Database('paragraphs.sqlite3', function() {
 		console.log("DB = ", db);
 	    console.log("createTable paragraphs");
-	    db.run("CREATE VIRTUAL TABLE IF NOT EXISTS webpages USING fts4(key, data);", function() {
-			allparas.observeChanges(onParaChange)
-		});
+	    db.run("CREATE VIRTUAL TABLE IF NOT EXISTS webpages USING fts4(key, data);", function() {});
 	});
   }).run();
 }
 
 Meteor.startup(function () {
 	setupSqlite();
+	allparas.observeChanges(onParaChange)
 });
 
