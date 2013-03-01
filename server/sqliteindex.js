54bd46a676fa3da8e081ac10b49d0588277b0c38
David Ascher <david.ascher@gmail.com>
2013-03-01 10:40:05 -0800

    / ‘server/sqliteindex.js’

--- server/sqliteindex.js
+++ server/sqliteindex.js
@@ -1,2 +1,13 @@
 // This keeps user-authored stuff indexed in sqlite
 
+var allparas = Paras.find();
+
+var onParaChange = {
+	added: function(id, fields) {
+		// console.log("added", id, fields);
+		Meteor._debug("added", id, fields);
+	}
+
+}
+
+allparas.observeChanges(onParaChange)
