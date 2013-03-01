417510302670c3209b3a1f0c2975bee1542cda8e
David Ascher <david.ascher@gmail.com>
2013-03-01 13:11:49 -0800

    / ‘server/sqliteindex.js’

--- server/sqliteindex.js
+++ server/sqliteindex.js
@@ -2,6 +2,25 @@
 
 var allparas = Paras.find();
 
+var db = new sqlite3.Database('webpages.sqlite3', createTable);
+function createTable() {
+    console.log("createTable webpages");
+    db.run("CREATE VIRTUAL TABLE webpages USING fts4(url, data);", mongoOpen)
+}
+
+db.run("INSERT INTO webpages VALUES ($url, $data)", {
+	$url: url,
+	$data: data
+})
+
+
+function closeDb() {
+    console.log("closeDb");
+    db.close();
+}
+
+
+
 var onParaChange = {
 	added: function(id, fields) {
 		Meteor._debug("added", id, fields);
